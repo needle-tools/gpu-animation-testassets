dbad00d60c169540f2fb408c65cf4af2cce5e823
Marcel Wiessler <marcel@gaisterhand.de>
2021-01-27 09:52:33 +0100

    add hdrp ifdef, normalize light component to set light back after opened hdrp proj

---
+++ package/Runtime/BetterShaders/NormalizeLighting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace BetterShaders
+{
+	[ExecuteAlways]
+	public class NormalizeLighting : MonoBehaviour
+	{
+#if !HDRP_INSTALLED
+		private Light light;
+		private void Update()
+		{
+			if (!light && !TryGetComponent(out light))
+			{
+				enabled = false;
+				return;
+			}
+
+			if (light.intensity > 100) light.intensity = 1;
+		}
+#endif
+	}
+}
